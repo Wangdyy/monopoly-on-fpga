ce897c424c9a61a149dfca23e488ed9842098517
jyan0302 <jyan0302@gmail.com>
2023-04-04 19:36:42 -0400

    added initgame test

--- src/unittests.c
+++ src/unittests.c
@@ -1,5 +1,13 @@
 #include "munit.h"
 #include <stdio.h>
+#include "stdbool.h"
+#include "squares.h"
+#include "players.h"
+#include "gamestate.h"
+#include "helpers.h"
+#include "display.h"
+#include "interface.h"
+#include "presets.h"
 
 MunitResult my_test(const MunitParameter params[], void *fixture)
 {
@@ -11,14 +19,34 @@ MunitResult my_test(const MunitParameter params[], void *fixture)
 static void *
 test_setup(const MunitParameter params[], void *user_data)
 {
-    printf("Setup");
-    return strdup("Hello world!");
+    return strdup("Hello, world!");
 }
 
 static void
 test_tear_down(void *fixture)
 {
-    printf("Teardown");
+    free(fixture);
+}
+
+MunitResult initGame_test(const MunitParameter params[], void *fixture)
+{
+    gamestate *game = (gamestate *)fixture;
+    initGame(game);
+    square Parkplace = game->board[37];
+    munit_assert_string_equal(Parkplace.name, "Park Place");
+    // munit_assert_int(Parkplace.name)
+    return MUNIT_OK;
+}
+
+static void *
+game_setup(const MunitParameter params[], void *user_data)
+{
+    return malloc(sizeof(gamestate));
+}
+
+static void
+game_tear_down(void *fixture)
+{
     free(fixture);
 }
 
@@ -28,6 +56,14 @@ MunitTest tests[] = {
         my_test,                /* test */
         test_setup,             /* setup */
         test_tear_down,         /* tear_down */
+        MUNIT_TEST_OPTION_NONE, /* options */
+        NULL                    /* parameters */
+    },
+    {
+        "/initgame",            /* name */
+        initGame_test,          /* test */
+        game_setup,             /* setup */
+        game_tear_down,         /* tear_down */
         MUNIT_TEST_OPTION_NONE, /* options */
         NULL                    /* parameters */
     },
